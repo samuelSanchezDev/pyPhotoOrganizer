eef217c386b60e681f21062ab868206a376c7eba
Samuel Sánchez Tabernero <SamuelSanchez.dev@gmail.com>
2024-07-18 05:34:50 +0200

    Incorporado sistema de logs.

--- pyPhotoOrganizer/file.py
+++ pyPhotoOrganizer/file.py
@@ -3,8 +3,9 @@ Módulo con la funcionalidad de manipular los archivos.
 """
 
 import datetime
+import logging
 import re
-from typing import List, Set, Tuple
+from typing import List, Set, Tuple, Union
 import os
 import hashlib
 
@@ -25,6 +26,8 @@ class PyFiles:
         :return: Lista con los path de los archivos con digest único.
         """
 
+        logging.debug('Filtrando archivos únicos.')
+
         # Se filtran los path que no corresponden a archivos.
         real_files: List[str] = list(filter(os.path.isfile, files))
 
@@ -32,6 +35,7 @@ class PyFiles:
         all_hash: Set[bytes] = set()
         unique_files: List[str] = []
         for file in real_files:
+            logging.debug('Verificando hash del archivo "%s"', file)
 
             my_hash = hashlib.sha1()
             with open(file, 'rb') as f:
@@ -39,20 +43,24 @@ class PyFiles:
                 digest = my_hash.digest()
 
             if digest not in all_hash:
+                logging.debug('Archivo aceptado.')
                 all_hash.add(digest)
                 unique_files.append(file)
 
         return unique_files
 
     @staticmethod
-    def date_from_name(file: str) -> Tuple[int, int, int]:
+    def date_from_name(file: str) -> Union[Tuple[int, int, int], None]:
         """
         Método para obtener la fecha del archivo del nombre (tiene que estar
         con la forma YYYYMMDD). La función no comprueba que el archivo exista.
 
         :param file: path del archivo.
-        :return: una tupla con el año, mes y día del archivo.
+        :return: una tupla con el año, mes y día del archivo, o None en caso
+        de que no se pueda obtener.
         """
+
+        logging.debug('Obteniendo fecha de archivo.')
 
         # Se obtiene el nombre del archivo.
         file_name = os.path.basename(file)
@@ -60,6 +68,8 @@ class PyFiles:
         # Se filtran los que no tienen en el nombre el patrón YYYYMMDD.
         result = re.search(DATE_FORMAT, file_name)
         if not result:
+            logging.debug('El archivo "%s" no sigue el formato YYYYMMDD.',
+                          file)
             return None
 
         # Se obtienen el año, mes y día.
@@ -68,12 +78,17 @@ class PyFiles:
         # Se verifica que son int.
         try:
             year, month, day = int(year), int(month), int(day)
-        except TypeError:  # Solo salta si la fecha no se puede convertir.
+        except TypeError as e:  # Solo salta si la fecha no se puede convertir.
+            logging.debug('No se ha podido convertir la fecha a entero.')
+            logging.debug('Excepción: %s', e)
             return None
 
         # Se verifica que la fecha sea real
         try:
             datetime.date(year, month, day)
             return year, month, day
-        except ValueError:  # Solo salta si la fecha no válida
+        except ValueError as e:  # Solo salta si la fecha no válida
+            logging.debug('La fecha %04d/%04d/%04d no es válida.', year, month,
+                          day)
+            logging.debug('Excepción: %s', e)
             return None
